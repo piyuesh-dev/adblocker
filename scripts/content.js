8bdfc68a66fce716fd90fff640aa864008e6c367
piyuesh <pmakkar@bayatree.com>
2023-01-16 20:11:02 +0530

    Done more changes to hide ad divs, and added few more urlfilters.

--- scripts/content.js
+++ scripts/content.js
@@ -1,98 +1,50 @@
-// findAdsIframe = () => {
-//     const iframes = document.querySelectorAll("iframe");
+let possibleAdsLabel = ["advertisement", "ad", "ads"];
+let adLabelObj = {
+    "advertisement":1,
+    "\"advertisement\"":1,
+    "ad":1,
+    "\"ad\"":1,
+    "ads":1,
+    "\"ads\"":1,
+}
 
-//     // find all iframes for google ads, and hide them...
-//     if (iframes && iframes.length) {
-//         for(var index=0; index<iframes.length; ++index) {
-//             let iframeEl = iframes[index];
-
-//             if(!iframeEl.getAttribute)
-//                 continue;
-//             let srcVal = iframeEl.getAttribute("src");
-//             let idVal = iframeEl.getAttribute("id");
-    
-//             // console.log(srcVal);
-//             if(srcVal && srcVal.match("googleads") ||
-//                 idVal && idVal.match("google_ads_iframe_")) {
-//                 // find top most parent of iframe which has no other child...
-//                 let topParentEl = findTopParent(iframeEl);
-//                 topParentEl.style.display = "none";
-//             }
-//         }
-//     }
-// };
-
-// findTaboolaContent = () => {
-//     const taboolaDivs = document.querySelectorAll(".trc_rbox_container");
-
-//     // find all sponsored contents for taboola, and hide them...
-//     if (taboolaDivs && taboolaDivs.length) {
-//         for(var index=0; index<taboolaDivs.length; ++index) {
-//             let divEl = taboolaDivs[index];
-
-//             // find top most parent of iframe which has no other child...
-//             let topParentEl = findTopParent(divEl);
-//             topParentEl.style.display = "none";
-//         }
-//     }
-// }
-
-// findFireworkContent = () => {
-//     const fireWorkDivs = document.querySelectorAll("fw-storyblock");
-
-//     // find all sponsored contents for taboola, and hide them...
-//     if (fireWorkDivs && fireWorkDivs.length) {
-//         for(var index=0; index<fireWorkDivs.length; ++index) {
-//             let divEl = fireWorkDivs[index];
-
-//             // find top most parent of iframe which has no other child...
-//             let topParentEl = findTopParent(divEl);
-//             topParentEl.style.display = "none";
-//         }
-//     }
-// }
-
-findAllEmptyAdDivs = () => {
+findAllEmptyAdDivs = (rootEl) => {
     // find all Divs with text or content "advertisement"...and hide them
-    let allAfterBeforeDivs = Array.from(document.querySelectorAll('div')).filter((divEl)=>{
+    let allAfterBeforeDivs = Array.from(rootEl.querySelectorAll('div,span')).filter((divEl)=>{
         let val1 = getComputedStyle(divEl, ':before').getPropertyValue('content').toLowerCase();
         let val2 = getComputedStyle(divEl, ':after').getPropertyValue('content').toLowerCase();
         let val3 = divEl.innerText && divEl.innerText.toLowerCase();
-        return (val1 == "\"advertisement\"" || val2 == "\"advertisement\"" || val3 == "advertisement");
+
+        return ((val1 in adLabelObj) || (val2 in adLabelObj) || (val3 in adLabelObj));
     });
+    console.dir(allAfterBeforeDivs);
+    // find divs with data-ad-slot or data-ad-unit...hide them....
+    allAfterBeforeDivs = allAfterBeforeDivs.concat(Array.from(
+        rootEl.querySelectorAll(`
+            div[data-ad-slot],
+            div[data-adsslot],
+            div[data-adslot],
+            div[data-ad-unit],
+            div[data-adcode],
+            div[id*="gpt_ad"],
+            div[id*="div-gpt-ad"]`)
+    ));
 
     // find all divs without any advertisement text...but they are empty...e.g. mint.com
-    // try to grep window.innerHTML for googletag.defineSlot.... and hide all those divs..
-    // var headContent = document.getElementsByTagName('head')[0].innerHTML;
-    // var bodyContent = document.getElementsByTagName('body')[0].innerHTML;
+    if (rootEl == document) {
+        var headContent = rootEl.getElementsByTagName('head')[0].innerHTML;
+        var bodyContent = rootEl.getElementsByTagName('body')[0].innerHTML;
 
-    // let regexAds = /defineSlot\(.*?('|")([^'"]*)('|")\)/g;
-    // let anySlots1=[];
-    // let anySlots2=[];
+        let tmpArr1 = findAdDivsAsPerRegex(headContent);
+        let tmpArr2 = findAdDivsAsPerRegex(bodyContent);
+        allAfterBeforeDivs = allAfterBeforeDivs.concat(tmpArr1, tmpArr2);
+    } else {
+        var elementContent = rootEl.innerHTML;
 
-    // while (tmpArr = regexAds.exec(headContent)) {
-    //     anySlots1.push(tmpArr);
-    // }
-    // while (tmpArr = regexAds.exec(bodyContent)) {
-    //     anySlots2.push(tmpArr);
-    // }
+        let tmpArr1 = findAdDivsAsPerRegex(elementContent);
+        allAfterBeforeDivs = allAfterBeforeDivs.concat(tmpArr1);
+    }
 
-    // if (anySlots1 && anySlots1.length) {
-    //     for(let j=0; j<anySlots1.length; ++j) {
-    //         let divId = anySlots1[j][2];
-    //         console.log("found div id---",divId);
-    //         let divEl = document.getElementById(divId);
-    //         divEl ? allAfterBeforeDivs.push(divEl): "";
-    //     }
-    // }
-    // if (anySlots2 && anySlots2.length) {
-    //     for(let j=0; j<anySlots2.length; ++j) {
-    //         let divId = anySlots2[j][2];
-    //         console.log("found div id---",divId);
-    //         let divEl = document.getElementById(divId);
-    //         divEl ? allAfterBeforeDivs.push(divEl): "";
-    //     }
-    // }
     if (allAfterBeforeDivs && allAfterBeforeDivs.length) {
         for(var index=0; index<allAfterBeforeDivs.length; ++index) {
             let divEl = allAfterBeforeDivs[index];
@@ -105,7 +57,34 @@ findAllEmptyAdDivs = () => {
     }
 }
 
-// find top most parent of iframe which has no other child...
+// https://regex101.com/r/GqKaIZ/1
+// https://regex101.com/r/fUVRvh/1
+// try to grep window.innerHTML for googletag.defineSlot.... and hide all those divs..
+function findAdDivsAsPerRegex(htmlString) {
+    let regexAds1 = /defineSlot\(.*?('|")([^'"]*)('|")\)/g;
+    let regexAds2 = /(googletag|gpt).display\(['"]{1}(.*?)['"]{1}\)/g;
+    let anySlots = []; // for regex matches...both regex has group2 as div id...
+    let foundDivs = [];
+
+    while (tmpArr = regexAds1.exec(htmlString)) {
+        anySlots.push(tmpArr);
+    }
+    while (tmpArr = regexAds2.exec(htmlString)) {
+        anySlots.push(tmpArr);
+    }
+
+    if (anySlots && anySlots.length) {
+        for(let j=0; j<anySlots.length; ++j) {
+            let divId = anySlots[j][2];
+            // console.log("found div id---",divId);
+            let divEl = document.getElementById(divId);
+            divEl ? foundDivs.push(divEl): "";
+        }
+    }
+    return foundDivs;
+}
+
+// find top most parent of iframe which has no other child...or only a script or text child...
 function findTopParent(childEl) {
     let parentEl = null;
 
@@ -130,24 +109,53 @@ function findTopParent(childEl) {
 function findOnlyTextChildParent(childEl) {
     let parentEl = childEl.parentElement;
     let childNodes = parentEl.children;
+    let child1Text = childNodes[0].innerText && childNodes[0].innerText.toLowerCase();
+    let child2Text = childNodes[1].innerText && childNodes[1].innerText.toLowerCase();
 
-    if((childNodes[0].innerText && childNodes[0].innerText.toLowerCase() == "advertisement") ||
-     (childNodes[1].innerText && childNodes[1].innerText.toLowerCase() == "advertisement")) {
-        return parentEl;
+    // or only script nodes...as 2nd child...
+    if (childNodes[0].nodeName.toLowerCase() == "script" || childNodes[1].nodeName.toLowerCase() == "script")
+        return findTopParent(parentEl);
+    
+    // only consider innerText value if other child is empty...., else just hide childEl
+    // so that we don't hide any valid non-ad content from html
+    if(possibleAdsLabel.find((element) => {return ((element==child1Text && child2Text=="") || (element==child2Text && child1Text==""));})) {
+        return findTopParent(parentEl);
     } else {
         return childEl;
     }
 }
 
-// listen for an event, whenever a iframe is added...
-document.addEventListener("load", ()=>{
-    // findAdsIframe();
-    // findTaboolaContent();
-    // findFireworkContent();
-});
+/*
+ * Adds css rules to hide known ads container...
+*/
+function addStyleSheetAds() {
+    var style = document.createElement('style');
+    style.innerHTML = `
+    #target {
+    color: blueviolet;
+    }
+    div[id^="gpt_ad"] {
+        display: none;
+    }
+    div[id*="div-gpt-ad"] {
+        display: none;
+    }
+    .ad-slot, .adSlot {
+        display:none !important;
+    }
+    ins[class="adsbygoogle"] {
+        display:none !important;
+    }
+    div[data-ad-slot] {
+        display:none !important;
+    }
+    `;
+    document.head.appendChild(style);  
+}
 
 // Select the node that will be observed for mutations
 const targetNode = document.body;
+let oldHref = document.location.href;
 
 // Options for the observer (which mutations to observe)
 const config = { attributes: false, childList: true, subtree: true };
@@ -155,26 +163,20 @@ const config = { attributes: false, childList: true, subtree: true };
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
-    // console.log("mutatin is ::::",mutation.type);
-
+    if (oldHref != document.location.href) {
+        // console.log(" url changed at the top....");
+        try {
+            findAllEmptyAdDivs(document);
+            oldHref = document.location.href;
+        } catch (ex) {
+            // do nothing....
+        }
+        break;
+    }
     if (mutation.type === 'childList') {
-    //   console.log('A child node has been added or removed.', mutation.addedNodes[0].nodeName);
       if(mutation.addedNodes && mutation.addedNodes.length) {
-            findAllEmptyAdDivs();
-        // for(let j in mutation.addedNodes) {
-        //     let nodeVal = mutation.addedNodes[j];
-        //     let nodeName = nodeVal.nodeName || "dummy";
-
-        //     // list src of all script nodes....and just block google ads script...
-        //     if(nodeName.toLowerCase() == "script") {
-        //         console.log(nodeVal.getAttribute("src"));
-        //     }
-        // }
+        // nothing...
       }
-
-    //   findAdsIframe();
-    //   findTaboolaContent();
-    //   findFireworkContent();
     } else if (mutation.type === 'attributes') {
       console.log(`The ${mutation.attributeName} attribute was modified.`);
     }
@@ -185,11 +187,11 @@ const callback = (mutationList, observer) => {
 const observer = new MutationObserver(callback);
 
 setTimeout(() => {
-    // findAdsIframe();
-    findAllEmptyAdDivs();
+    addStyleSheetAds();
+    findAllEmptyAdDivs(document);
     // Start observing the target node for configured mutations
-    // observer.observe(targetNode, config);
-}, 4000);
+    observer.observe(targetNode, config);
+}, 500);
 
 // Later, you can stop observing
 // observer.disconnect();
